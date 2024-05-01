346c471125960e970eee543b37d2d267389f7189
imanwarsame <iwarsame38@gmail.com>
2024-05-01 18:52:46 +0100

    Undid changing vite config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -22,15 +22,4 @@ export default defineConfig({
 		host: true, // Open to local network and display URL
 		open: !('SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env), // Open if it's not a CodeSandbox
 	},
-	build: {
-		rollupOptions: {
-			output: {
-				manualChunks(id) {
-					if (id.includes('node_modules')) {
-						return 'vendor';
-					}
-				},
-			},
-		},
-	},
 });
